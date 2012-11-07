2ed334138d224ff310a8628b313e399aab17372f
Helen <helen@helenweng.com>
2012-11-06 19:33:31 -0800

    blargh

--- sample-ntwitter/newapp.js
+++ sample-ntwitter/newapp.js
@@ -1,8 +1,21 @@
-var app = require('express').createServer(), keys = require('secrets'), twitter = require('ntwitter'); 
+var app = require('express').createServer(), keys = require('./secrets'), twitter = require('ntwitter'); 
 app.listen(3000); 
-var twit = new twitter({ consumer_key: keys.CONSUMER_KEY, consumer_secret: keys.CONSUMER_SECRET, access_token_key: keys.ACCESS_TOKEN, access_token_secret: keys.ACCESSTOKENSECRET
+
+var twit = new twitter({ consumer_key: keys.CONSUMER_KEY, consumer_secret: keys.CONSUMER_SECRET, access_token_key: keys.ACCESS_TOKEN, access_token_secret: keys.ACCESS_TOKEN_SECRET
 });
 
-twit.stream('statuses/filter', { 'locations':''}, function(stream) { stream.on('data', 
+fs = require('fs')
+locations = ""
+fs.readFile('./ca-polling-locations-gps-bounding-boxes-string.txt', 'utf-8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  locations = data;
+});
+console.log("I was here");
+console.log(locations);
+twit.stream('statuses/filter', { 'locations': locations}, function(stream) { stream.on('data', 
     function (data) { console.log(data.user.screen_name + ": " + data.text); }); });
+
+
 

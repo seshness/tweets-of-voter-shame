ba0634bfde510b21281bce1a25fc4e20549e70ff
Seshadri Mahalingam <seshadri@berkeley.edu>
2012-11-06 22:41:08 -0800

    statuses/oembed.json

--- public/application.js
+++ public/application.js
@@ -1,4 +1,12 @@
-var socket = io.connect('http://localhost:8000/');
-socket.on('new-tweet', function (data) {
-  console.log(data);
+$(function() {
+  var socket = io.connect('http://localhost:8000/');
+  socket.on('new-tweet', function (data) {
+    console.log(data);
+    var url = 'https://api.twitter.com/1/statuses/oembed.json?' +
+      $.param({id: data.id_str, align: 'center', callback: 'tweet'});
+    $.ajax(url, { dataType: 'jsonp', jsonpCallback: 'tweet' })
+      .done(function(twitter_response) {
+        console.log(twitter_response);
+      });
+  });
 });
